cab7b92439f8633114e696c4323355ff7f5debef
Pianosalesman <andreydavydov1@gmail.com>
2023-06-26 21:55:48 +0300

    Работающая версия чат-игры (версия 4)

--- Chat-game.py
+++ Chat-game.py
@@ -25,18 +25,18 @@ class BasicAuto(TrafficMixin):  # Класс движущегося авто
         self.name = name
         self.life = life
 
-    def __del__(self):  # Деструктор, вызывающий взрыв
-        if self.life <= 0:
-            self.car_explosion()
-
     def car_explosion(self):  # Взрыв автомобиля
         smoke = MovingSmoke()
         print(f"Машина {self.name} взорвалась. Дымом покрылась площадь {smoke.square_smoke}")
+
+    def __del__(self):
+        pass
 
 
 class MilitaryAuto(BasicAuto):  # Класс для военной машины
     Weapon = enum.Enum('Weapon', [('MACHINE_GUN', 2), ('HEAVY_MACHINE_GUN', 5), ('GRENADE_LAUNCHER', 10)])
     Machine_gun = Weapon['MACHINE_GUN']
+    Grenade_launcher = Weapon['GRENADE_LAUNCHER']
 
     def shooting(self, target: 'BasicAuto'):  # Метод для стрельбы
         target.life -= self.Machine_gun.value
@@ -58,13 +58,21 @@ class PeacefulAuto(BasicAuto):  # Класс для мирного авто
 
 if __name__ == '__main__':
     peaceful_car1 = PeacefulAuto(4, 'Ford', life=10)
-    peaceful_car1.move(6)
     peaceful_car2 = PeacefulAuto(3, 'Lada', life=10)
-    peaceful_car2.move(15)
     peaceful_car3 = PeacefulAuto(5, 'Opel', life=10)
+
+    peaceful_car1.move(6)
+    peaceful_car2.move(15)
     peaceful_car3.move(25)
-    peaceful_car3.__setattr__('life', 0)
 
     Hummer = MilitaryAuto('Hummer')
     Hummer.shooting(peaceful_car1)
     Hummer.shooting(peaceful_car2)
+    peaceful_car3.__setattr__('life', 0)
+
+    car_list = [peaceful_car1, peaceful_car2, peaceful_car3]
+    for car in car_list:
+        if car.life <= 0:
+            car.car_explosion()
+            car_list.remove(car)
+            break
